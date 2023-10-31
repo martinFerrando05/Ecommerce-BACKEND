3c125de0984965c5385b5667d70f55c2c90aacf7
martinferrando2005 <ferrandomartin6@gmail.com>
2023-10-31 12:20:35 -0300

    channnges

--- api/config/db/index.js
+++ api/config/db/index.js
@@ -1,11 +1,15 @@
 // configuracion de la DB
 require("dotenv").config();
 const Sequelize = require("sequelize");
-const db = new Sequelize(process.env.DB_NAME, null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  "ecommerce_mlf2",
+  "4AG9jj84HuYoQcePBAYdDJIdVHCpTIvh",
+  {
+    host: "localhost",
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = db;
-
